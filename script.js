e685999db2db8eabee363013a501c4eedc71b17a
juanchizambo-max <juanchizambo@gmail.com>
2026-01-16 21:16:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-const openBtn = document.getElementById("openModal");
-const closeBtn = document.getElementById("closeModal");
-const overlay = document.getElementById("overlay");
-
-openBtn.addEventListener("click", () => {
-  overlay.style.display = "flex";
-});
-
-closeBtn.addEventListener("click", () => {
-  overlay.style.display = "none";
-});
+alert("Bienvenues aux aventures de 6ème");
